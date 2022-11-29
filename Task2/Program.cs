3cb3013864505cf60e3f4c7c8373af7370c2700e
KrisNes <kris.nesterova28@yandex.ru>
2022-11-30 00:13:51 +0300

    добавлено решение второй задачи

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -7,16 +7,32 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-Console.WriteLine("Введите 2 целых числа: ");
-var a = Decimal.Parse(Console.ReadLine());
-var b = Decimal.Parse(Console.ReadLine());
-Console.WriteLine("Наибольшее число: ");
+// Console.WriteLine("Введите 2 целых числа: ");
+// var a = Decimal.Parse(Console.ReadLine());
+// var b = Decimal.Parse(Console.ReadLine());
+// Console.WriteLine("Наибольшее число: ");
+// if (a > b)
+// Console.WriteLine(a);
+// else 
+// Console.WriteLine(b);
+// Console.WriteLine("Наименьшее число: ");
+// if (a < b)
+// Console.WriteLine(a);
+// else 
+// Console.WriteLine(b);
+
+Console.Write("Введите первое целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
 if (a > b)
-Console.WriteLine(a);
-else 
-Console.WriteLine(b);
-Console.WriteLine("Наименьшее число: ");
-if (a < b)
-Console.WriteLine(a);
-else 
-Console.WriteLine(b);
+{
+    Console.WriteLine("Наибольшее число " + a);
+    Console.WriteLine("Наименьшее число " + b);
+}
+else
+{
+    Console.WriteLine("Наибольшее число " + b);
+    Console.WriteLine("Наименьшее число "  + a);
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая 
+// 1. принимает на вход три числа и 
+// 2. выдаёт максимальное из этих чисел.
+
+// Например: 
+// 2, 3, 7 -> 7
+// 44, 5, 78 -> 78
+// 22, 3, 9 -> 22
+
+Console.WriteLine("Введите 3 целых числа: ");
+var a = Decimal.Parse(Console.ReadLine());
+var b = Decimal.Parse(Console.ReadLine());
+var c = Decimal.Parse(Console.ReadLine());
+
+int max = a;
+if (b > max)
+    max = b;
+if (c > max)
+    max = c;
+Console.WriteLine("Наибольшее из введённых чисел -> " + max);
