b26265ac9b12ea6985119bd83174506bc4ac2a0e
KrisNes <kris.nesterova28@yandex.ru>
2022-11-30 01:14:56 +0300

    добавлена папка для первого семинара

---
+++ Seminar1HW/Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая 
+// 1. на вход принимает два числа и 
+// 2. ввыдаёт, какое число большее, а какое меньшее.
+
+// Например: 
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.WriteLine("Введите 2 целых числа: ");
+// var a = Decimal.Parse(Console.ReadLine());
+// var b = Decimal.Parse(Console.ReadLine());
+// Console.WriteLine("Наибольшее число: ");
+// if (a > b)
+// Console.WriteLine(a);
+// else 
+// Console.WriteLine(b);
+// Console.WriteLine("Наименьшее число: ");
+// if (a < b)
+// Console.WriteLine(a);
+// else 
+// Console.WriteLine(b);
+
+Console.Write("Введите первое целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+if (a > b)
+{
+    Console.WriteLine("Наибольшее число " + a);
+    Console.WriteLine("Наименьшее число " + b);
+}
+else
+{
+    Console.WriteLine("Наибольшее число " + b);
+    Console.WriteLine("Наименьшее число "  + a);
+}
---
+++ Seminar1HW/Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая 
+// 1. принимает на вход три числа и 
+// 2. выдаёт максимальное из этих чисел.
+
+// Например: 
+// 2, 3, 7 -> 7
+// 44, 5, 78 -> 78
+// 22, 3, 9 -> 22
+
+Console.Write("Введите первое целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье целое число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine("Наибольшее из введённых чисел -> " + max);
